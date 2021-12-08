2eda382d830bd3c689a0631b74b92d0179eb6530
corbs <corbtastik@gmail.com>
2021-12-07 20:51:33 -0600

    added mongodb-all, mongodb 4.4

--- deployments/mongodb/deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-kubectl create ns mongodb
-kubectl apply -n mongodb -f pvc.yml
-kubectl apply -n mongodb -f secret.yml
-kubectl apply -n mongodb -f deployment.yml
-kubectl apply -n mongodb -f service.yml
--- deployments/mongodb/undeploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-kubectl delete deployment mongodb-standalone -n mongodb
-kubectl delete secret mongodb-admin-creds -n mongodb
-kubectl delete services mongodb-standalone -n mongodb
-kubectl delete pvc mongodb-pvc8gb -n mongodb
